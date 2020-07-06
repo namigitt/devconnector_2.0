11f6eba04a6771f86d43c45d822c1f711a87f93d
namratapu <32532906+namratapu@users.noreply.github.com>
2020-07-05 22:02:10 -0700

    adding the file changes to master

---
+++ client/src/components/dashboard/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome Home</p>
+      </div>
+    )
+  }
+}
+
+Home.propTypes = {}
+
+export default Home
